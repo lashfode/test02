9123cd3edbea2410072dc86a1685332767834760
Daehun <mandak1306@gmail.com>
2024-04-16 13:40:45 +0900

    5555

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//Version/0.2
+//Version/0.3
 namespace place22
 {
     public partial class Form1 : Form
